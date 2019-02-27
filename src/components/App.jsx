98ab597a42280473b77076daac09ae1585cd984d
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-02-27 10:45:17 -0300

    first commit. Webpack implemented

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../css/main.scss';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>IT WORKS!!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
